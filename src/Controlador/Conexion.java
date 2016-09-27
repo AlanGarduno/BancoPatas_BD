d16a58c19739ebb61bbc9ab45ad4479b4503409b
agv_l <agv_l@ALAN-PC>
2016-09-26 23:25:33 -0500

    Conexion agregada

--- src/Controlador/Conexion.java
+++ src/Controlador/Conexion.java
@@ -1,7 +1,48 @@
 package Controlador;
 
 //Se conecta a Mysql 
-public class Conexion 
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class Conexion 
 {
+    private static Connection conn;
+    private static final String driver="com.mysql.jdbc.Driver";
+    private static final String user = "root";
+    private static final String pass = "0709";
+    private static final String url = "jdbc:mysql://localhost:3306/prueba";
     
+    public Conexion ()
+    {
+        conn = null;
+        try
+        {
+          Class.forName(driver);
+          conn = (Connection) DriverManager.getConnection(url, user, pass);
+          if(conn != null)
+          {
+              System.out.println("Conexion establecida");
+          }
+        }
+        catch(ClassNotFoundException | SQLException e)
+        {
+            System.out.println("Error al conectar");
+        }
+    }
+    
+    public Connection getConnection()
+    {
+        return conn;
+    }
+    
+    public void desconectar()
+    {
+        conn = null;
+        if(conn == null)
+        {
+            System.out.println("Conexion terminada");
+        }
+    }
 }
